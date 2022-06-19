6869bf06aeaf359877de83f7410b40779d48ce9c
sample <sample@example.com>
2022-06-20 01:16:51 +0800

    env

--- system/env.sh
+++ system/env.sh
@@ -15,5 +15,5 @@ export HOMEBREW_BREW_GIT_REMOTE=\"https://mirrors.tuna.tsinghua.edu.cn/git/homeb
 export HOMEBREW_CORE_GIT_REMOTE=\"https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-core.git\"
 
 alias p=\"export https_proxy=http://127.0.0.1:7890 http_proxy=http://127.0.0.1:7890 all_proxy=socks5://127.0.0.1:7890\"
-" >> $pt;
+" > $pt;
 done
