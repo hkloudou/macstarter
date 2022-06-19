90dcb49cbdda79b21bd5b8745a6068234e5b7ae3
sample <sample@example.com>
2022-06-20 04:40:30 +0800

    flutter

---
+++ installers/flutter.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+###############################################################################
+# Flutter
+###############################################################################
+#######color code########
+red="31m"
+green="32m"
+yellow="33m"
+blue="36m"
+fuchsia="35m"
+
+color_echo() {
+    echo "\033[$1${@:2}\033[0m"
+}
+if [ ! -d "$HOME/Developments/flutter" ]; then
+    echo "⚠️$(color_echo ${green} installers/flutter installing...)"
+    mkdir -p ~/Developments
+    mkdir -p ~/Developments/flutter
+    curl -SL https://storage.flutter-io.cn/flutter_infra_release/releases/stable/macos/flutter_macos_3.0.2-stable.zip | bsdtar --strip-components 1 -xf - -C ~/Developments/flutter
+fi
+echo "✅$(color_echo ${green} installers/flutter installed)\n"
