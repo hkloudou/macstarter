a5bf9e75b9295c7458a1c0cf2a3d0f3e99877bde
sample <sample@example.com>
2022-06-20 01:14:23 +0800

    env

--- system/env.sh
+++ system/env.sh
@@ -5,3 +5,15 @@
 ###############################################################################
 echo "\033[32mConfig env...\033[0m"
 
+# golang flutter brew
+for pt in ~/.zprofile ~/.profile ~/.bash_profile; 
+do  
+test -r $pt && echo "
+export PATH=\$PATH:`echo ~`/go/bin
+export PATH=\$PATH:`echo ~`/Developments/flutter/bin
+export HOMEBREW_BREW_GIT_REMOTE=\"https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/brew.git\"
+export HOMEBREW_CORE_GIT_REMOTE=\"https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-core.git\"
+
+alias p=\"export https_proxy=http://127.0.0.1:7890 http_proxy=http://127.0.0.1:7890 all_proxy=socks5://127.0.0.1:7890\"
+" >> $pt;
+done
